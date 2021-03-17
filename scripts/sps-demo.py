af78c5690ba698222b3467978f394ac6789826a5
Johnny Greco <jgreco.astro@gmail.com>
2021-03-16 23:04:43 -0400

    updated sps demo figure

--- scripts/sps-demo.py
+++ scripts/sps-demo.py
@@ -15,19 +15,15 @@ zpt_convert = artpop.load_zero_point_converter()
 ###############################################################################
 # Sun AB and Vega magnitudes for converting to Solar luminosities
 ###############################################################################
-U_sun_ab = 6.35
-U_sun_vega = 5.55
+V_sun_ab = 4.81
+V_sun_vega = 4.81
 
-B_sun_ab = 5.35
-B_sun_vega = 5.46
-
-R_sun_ab = 4.60
-R_sun_vega = 4.41
+I_sun_ab = 4.52
+I_sun_vega = 4.10
 
 sun_mag = dict(
-    U=dict(vega=U_sun_vega, ab=U_sun_ab),
-    B=dict(vega=B_sun_vega, ab=B_sun_ab),
-    R=dict(vega=R_sun_vega, ab=R_sun_ab),
+    V=dict(vega=V_sun_vega, ab=V_sun_ab),
+    I=dict(vega=I_sun_vega, ab=I_sun_ab)
 )
 
 # Solar luminosity as function of filter, absolute mag, and magnitude system
@@ -36,7 +32,7 @@ L_sun = lambda m, band, system: 10**(0.4*(sun_mag[band][system] - m))
 
 
 ###############################################################################
-# Calculate SSP mass, mags, and SBF mags using ArtPop (takes ~8 minutes)
+# Calculate SSP mass, mags, and SBF mags using ArtPop (takes ~6 minutes)
 ###############################################################################
 imf = 'kroupa'
 phot_system = 'UBVRIplus'
@@ -46,27 +42,23 @@ log_ages = artpop.MISTIsochrone._log_age_grid[40:-3]
 mass = {}
 
 # SSP magnitudes
-U = {}
-B = {}
-R = {}
+V = {}
+I = {}
 
 # SSP SBF magnitudes
-Ubar = {}
-Bbar = {}
-Rbar = {}
+Vbar = {}
+Ibar = {}
 
 for feh in [-1.5, 0.0]:
     print(f'[Fe/H] = {feh}')
 
     mass[feh] = []
 
-    U[feh] = []
-    B[feh] = []
-    R[feh] = []
+    V[feh] = []
+    I[feh] = []
 
-    Ubar[feh] = []
-    Bbar[feh] = []
-    Rbar[feh] = []
+    Vbar[feh] = []
+    Ibar[feh] = []
 
     for log_age in log_ages:
 
@@ -74,23 +66,19 @@ for feh in [-1.5, 0.0]:
 
         mass[feh].append(mist.ssp_surviving_mass('kroupa'))
 
-        U[feh].append(mist.ssp_mag('Bessell_U', imf))
-        B[feh].append(mist.ssp_mag('Bessell_B', imf))
-        R[feh].append(mist.ssp_mag('Bessell_R', imf))
+        V[feh].append(mist.ssp_mag('Bessell_V', imf))
+        I[feh].append(mist.ssp_mag('Bessell_I', imf))
 
-        Ubar[feh].append(mist.ssp_sbf_mag('Bessell_U', imf))
-        Bbar[feh].append(mist.ssp_sbf_mag('Bessell_B', imf))
-        Rbar[feh].append(mist.ssp_sbf_mag('Bessell_R', imf))
+        Vbar[feh].append(mist.ssp_sbf_mag('Bessell_V', imf))
+        Ibar[feh].append(mist.ssp_sbf_mag('Bessell_I', imf))
 
     mass[feh] = np.array(mass[feh])
 
-    U[feh] = np.array(U[feh]) + zpt_convert.to_ab('Bessell_U')
-    B[feh] = np.array(B[feh]) + zpt_convert.to_ab('Bessell_B')
-    R[feh] = np.array(R[feh]) + zpt_convert.to_ab('Bessell_R')
+    V[feh] = np.array(V[feh]) + zpt_convert.to_ab('Bessell_V')
+    I[feh] = np.array(I[feh]) + zpt_convert.to_ab('Bessell_I')
 
-    Ubar[feh] = np.array(Ubar[feh]) + zpt_convert.to_ab('Bessell_U')
-    Bbar[feh] = np.array(Bbar[feh]) + zpt_convert.to_ab('Bessell_B')
-    Rbar[feh] = np.array(Rbar[feh]) + zpt_convert.to_ab('Bessell_R')
+    Vbar[feh] = np.array(Vbar[feh]) + zpt_convert.to_ab('Bessell_V')
+    Ibar[feh] = np.array(Ibar[feh]) + zpt_convert.to_ab('Bessell_I')
 ###############################################################################
 
 
@@ -110,19 +98,17 @@ for feh in [0.0, -1.5]:
 
     label = lambda b: {0.0: '', -1.5: '$M / L_' + b + '$'}[feh]
 
-    m_l = mass[feh] / L_sun(R[feh], 'R', 'ab')
-    ax[0].plot(10**log_ages, m_l, c='tab:red', label=label('R'), **kw)
+    m_l = mass[feh] / L_sun(I[feh], 'I', 'ab')
+    ax[0].plot(10**log_ages, m_l, c='tab:red', label=label('I'), **kw)
 
-    m_l = mass[feh] / L_sun(B[feh], 'B', 'ab')
-    ax[0].plot(10**log_ages, m_l, c='tab:blue', label=label('B'), **kw)
-
-    m_l = mass[feh] / L_sun(B[feh], 'U', 'ab')
-    ax[0].plot(10**log_ages, m_l, c='blueviolet', label=label('U'), **kw)
+    m_l = mass[feh] / L_sun(V[feh], 'V', 'ab')
+    ax[0].plot(10**log_ages, m_l, c='tab:blue', label=label('V'), **kw)
 
     sign = '' if feh >= 0 else '$-$'
-    ax[1].plot(10**log_ages, U[feh] - R[feh], c='k',  **kw)
-    ax[2].plot(10**log_ages, B[feh] - R[feh], c='k', **kw)
-    ax[3].plot(10**log_ages, Bbar[feh] - Rbar[feh], c='k',
+    ax[1].plot(10**log_ages, V[feh] - I[feh], c='k',  **kw)
+    ax[2].plot(10**log_ages, Ibar[feh], c='tab:red', label=label('I'),**kw)
+    ax[2].plot(10**log_ages, Vbar[feh], c='tab:blue', label=label('V'), **kw)
+    ax[3].plot(10**log_ages, Vbar[feh] - Ibar[feh], c='k',
                label=f'{sign}{abs(feh)}', **kw)
 
 ax[0].set(yscale='log')
@@ -130,18 +116,21 @@ ax[0].set_ylim(ymax=10)
 ax[0].set_ylabel('$M / L$ [$M_\odot/L_\odot$]', fontsize=25, labelpad=4)
 ax[0].set_yticks([0.1, 1, 10])
 ax[0].set_yticklabels([0.1, 1, 10])
-ax[0].legend(loc='upper left', fontsize=20, frameon=False, borderaxespad=0.9)
+ax[0].legend(loc='upper left', fontsize=22, frameon=False, borderaxespad=0.9)
 
-ax[1].set_ylim(-0.8, 3)
-ax[2].set_ylim(-0.7, 1.5)
-
-ax[1].set_ylabel('$U - R$', fontsize=25, labelpad=8)
-ax[2].set_ylabel('$B - R$', fontsize=25, labelpad=-4)
-ax[3].set_ylabel('$\overline{B} - \overline{R}$', fontsize=25, labelpad=8)
+ax[1].set_ylabel('$V - I$', fontsize=25, labelpad=-12)
+ax[2].set_ylabel('SBF Magnitude', fontsize=25)
+ax[3].set_ylabel('$\overline{V} - \overline{I}$', fontsize=25, labelpad=10)
 
 ax[3].legend(loc='lower right', frameon=True, title='[Fe/H]', handlelength=1.7,
              handletextpad=0.5, fontsize=21, title_fontsize=20,
              borderaxespad=0.8)
+
+ax[2].legend(loc='lower left', handlelength=1.7,
+             handletextpad=0.5, fontsize=21, title_fontsize=22,
+             borderaxespad=0.8)
+
+ax[2].invert_yaxis()
 
 for i in range(4):
     ax[i].minorticks_on()
